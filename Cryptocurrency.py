375238d47c79bf6fa0333297b0da66151a98d39d
sanekua <sasha-kuzmyk@mail.ru>
2021-04-02 01:08:41 +0300

    Changed cryptocurrency file, add monitoring, modeling data

--- Cryptocurrency.py
+++ Cryptocurrency.py
@@ -6,21 +6,39 @@ import json
 import urllib
 
 
+def create_empty_files():
+    with open('modelling_api_data.json', 'w') as modeling_data, \
+            open('monitoring_api_data.json', 'w') as monitoring_data:
+        modeling_data.write('Modeling Data' + '\n')
+        monitoring_data.write('Monitoring Data' + '\n')
+
+
+def save_to_json(static_data, dynamic_data):
+    with open('modelling_api_data.json', 'a') as modeling_data, \
+            open('monitoring_api_data.json', 'a') as monitoring_data:
+        json.dump(static_data, modeling_data, sort_keys=True, indent=4)
+        json.dump(dynamic_data, monitoring_data, sort_keys=True, indent=2)
+        modeling_data.write('\n')
+        monitoring_data.write('\n')
+
+
 def callback_body(body):
+    create_empty_files()
     api_data = json.loads(body).get('data')
+    #print api_data[0].get(u'name'), len(api_data)
     for i in range(len(api_data)):
         value = api_data[i]
-        static_data = {'name': value.get(u'name'),
-                       'data_added': value.get(u'date_added'),
-                       'slug': value.get(u'slug'),
-                       'symbol': value.get(u'symbol'), }
-        dynamic_data = {'Info Crypto changes': value.get(u'quote'),
+        static_data = {value.get(u'name'): {'name': value.get(u'name'),
+                                            'data_added': value.get(u'date_added'),
+                                            'slug': value.get(u'slug'),
+                                            'symbol': value.get(u'symbol')}}
+        dynamic_data = {'Cryptocurrency name': value.get(u'name'),
+                        'Information about Cryptocurrency': value.get(u'quote'),
                         'last_updated': value.get(u'last_updated')}
-        logging.info('Static data: {}, Dynamic data : {} '.format(
+        logging.info('Static data: {}, Dynamic data : {}'.format(
                                                                 static_data, dynamic_data))
-        with open('cryptocurrency.json', 'a') as f:
-            json.dump((static_data, dynamic_data), f)
-            f.write('\n')
+        if value.get(u'name') in CRYPTOCURRENCY_LIST:
+            save_to_json(static_data, dynamic_data)
 
 
 def callback_request(response):
@@ -30,6 +48,14 @@ def callback_request(response):
 
 
 if __name__ == '__main__':
+    """
+    You can add to CRYPTOCURRENCY_LIST cryptocurrency in which you are 
+    interested. Also you can add limit(limit=20) to parameters to display 
+    first 50 Cryptocurrency on https://coinmarketcap.com/ website (default=50)
+    Modelling and Monitored data about chosen cryptocurrency saved in 
+    modeling_data.json and monitoring_data.json. If you want to see rating list
+    data saved to cryptoccurency.log file
+    """
     parameters = {
         'start': '1',
         'limit': '50',
@@ -38,8 +64,9 @@ if __name__ == '__main__':
     url_with_parameters = urllib.urlencode(parameters)
     URL = url + url_with_parameters
     agent = Agent(reactor)
+    CRYPTOCURRENCY_LIST = [u'Bitcoin', u'Binance Coin', u'Cardano', u'Ethereum']
     logging.basicConfig(filename='cryptocurrency.log', level=logging.INFO,
-                        format='%(asctime)s %(message)s', filemode='a')
+                        format='%(asctime)s %(message)s', filemode='w')
     d = agent.request(
         'GET',
         URL,
@@ -52,7 +79,5 @@ if __name__ == '__main__':
 
     def cbShutdown(ignored):
         reactor.stop()
-
-
     d.addBoth(cbShutdown)
     reactor.run()
